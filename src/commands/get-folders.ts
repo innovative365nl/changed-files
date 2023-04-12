396b5b96a06f65141697d11effa5716c6be81ead
MatthijsvanderPlas <mvdplas84@ziggo.nl>
2023-04-12 14:21:33 +0200

    Full path

--- src/commands/get-folders.ts
+++ src/commands/get-folders.ts
@@ -3,9 +3,7 @@ import { ICommand, File } from './command';
 export class GetFoldersCommand implements ICommand {
   run(files: File[]): File[] {
     return files
-      .map(x => x)
-      .filter(file => file.filename.includes("/"))
-    
+      .map(x => x)    
     }
 }
 
